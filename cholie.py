e51ef443e3eab7b4468539660ce9f9e841e25a8e
Yorgos <athanasiadis@birc.au.dk>
2017-01-18 18:42:33 +0100

    Update cholie.py

--- cholie.py
+++ cholie.py
@@ -8,7 +8,7 @@ def cholie(g, normalize=False):
     resolved = False
     while not resolved:
         try:
-            h = pd.DataFrame.sample(g, n_snps, replace=False)
+            h = pd.DataFrame.sample(g, n_snps)
             grm = sp.dot(h.T, h) / n_snps
             cho = cholesky(pinv(grm))
             resolved = True
