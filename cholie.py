8c604825b36897b1741927a056d624b3f908886a
Yorgos <athanasiadis@birc.au.dk>
2017-01-18 18:37:13 +0100

    Add files via upload

---
+++ cholie.py
@@ -0,0 +1,17 @@
+def cholie(g, normalizge=False):
+    """
+    Calculate the Cholesky decomposition of a GRM based on a genotype matrix g
+
+    This function simply shuffles the rows in g until no error is returned
+    """
+    n_snps, n_indiv = np.shape(g)
+    resolved = False
+    while not resolved:
+        try:
+            h = pd.DataFrame.sample(g, n_snps, replace=False)
+            grm = sp.dot(h.T, h) / n_snps
+            cho = cholesky(pinv(grm))
+            resolved = True
+            return chocholi
+        except:
+            pass
